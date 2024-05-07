98e999496082a0d9fca3a8c69f659236841b87e4
alex-delia <adelia3@uwo.ca>
2024-05-07 15:58:00 -0400

    create layout and styles for Cart page

---
+++ src/components/Cart.jsx
@@ -0,0 +1,55 @@
+import { useOutletContext } from "react-router-dom";
+import CartCard from "./CartCard";
+import styles from '../styles/Cart.module.css';
+
+const Cart = () => {
+    const [cart, setCart] = useOutletContext();
+    let total = 0;
+
+    const removeFromCart = (id) => {
+        const updatedCart = cart.filter(item => item.id !== id);
+        setCart(updatedCart);
+    };
+
+    const incrementQuantity = (id) => {
+        const updatedCart = cart.map(item =>
+            item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+        setCart(updatedCart);
+    };
+
+    const decreaseQuantity = (id) => {
+        const updatedCart = cart.map(item =>
+            item.id === id ? { ...item, quantity: Math.max(1, item.quantity - 1) } : item
+        );
+        setCart(updatedCart);
+    };
+
+    const cartItems = cart.map((item) => {
+        total += item.price * item.quantity;
+        return <CartCard
+            key={item.id}
+            item={item}
+            increment={incrementQuantity}
+            decrement={decreaseQuantity}
+            remove={removeFromCart}
+        />;
+    });
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.header}>Cart</h1>
+            <div className={styles.columnHeaders}>
+                <p className={styles.product}>Product</p>
+                <p>Unit Price</p>
+                <p>Quantity</p>
+                <p>Total Price</p>
+                <p>Remove From Cart</p>
+            </div>
+            <div>{cartItems}</div>
+            <h3 className={styles.total}>Total: ${total.toFixed(2)}</h3>
+        </div>
+    );
+};
+
+export default Cart;
