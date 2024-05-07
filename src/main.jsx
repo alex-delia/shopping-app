ae9c1c1cb3ada2e5d3cc7d95c683607d9c6abcea
alex-delia <adelia3@uwo.ca>
2024-05-07 16:05:33 -0400

    create cart route and import default stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Home from './components/Home.jsx';
 import Shop from './components/Shop.jsx';
+import Cart from './components/Cart.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
 
 const router = createBrowserRouter([
   {
@@ -17,8 +19,11 @@ const router = createBrowserRouter([
       {
         path: 'shop',
         element: <Shop />
+      },
+      {
+        path: 'cart',
+        element: <Cart />
       }
-
     ]
   }
 ]);
