85386aab8824cbd518352433264482f5c0187181
alex-delia <adelia3@uwo.ca>
2024-05-07 16:07:25 -0400

    create navbar for header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
+import styles from '../styles/NavBar.module.css';
+import PropTypes from 'prop-types';
 
-const NavBar = () => {
+const CartIcon = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='shop'>Shop</Link></li>
-            </ul>
-        </nav>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+        </svg>
     );
+};
+
+const NavBar = ({ numItems }) => {
+    return (
+        <nav className={styles.header}>
+            <h1 className={styles.title}>Zander Designs</h1>
+            <ul className={styles.navbar}>
+                <li className={styles.navItem}><Link to='/' className={styles.link}>Home</Link></li>
+                <li className={styles.navItem}><Link to='shop' className={styles.link}>Shop</Link></li>
+                <li className={styles.navItem}><Link to='cart' className={styles.link}><CartIcon /> {numItems}</Link></li>
+            </ul>
+        </nav >
+    );
+};
+
+NavBar.propTypes = {
+    numItems: PropTypes.number
 };
 
 export default NavBar;
