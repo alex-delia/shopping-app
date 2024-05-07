c2055dcd62c8942c4a2efd7776f777238eaff491
alex-delia <adelia3@uwo.ca>
2024-05-07 16:08:14 -0400

    create new state to track items in cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar.jsx";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
+  const [cart, setCart] = useState([]);
+
+  let totalItems = 0;
+  if (cart.length > 0) {
+    totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+  }
+
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <NavBar numItems={totalItems} />
+      <Outlet context={[cart, setCart]} />
     </>
   );
 };
