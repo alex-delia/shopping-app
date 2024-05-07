7ce4a514f4e235ec3a04f0aec6ea49c07e692153
alex-delia <adelia3@uwo.ca>
2024-05-07 15:58:58 -0400

    creater layout and styles for shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,65 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import Card from "./Card.jsx";
+import styles from '../styles/Shop.module.css';
+
+const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error('Server Error');
+                }
+                return response.json();
+            })
+            .then((response) => setProducts(response))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return { products, error, loading };
+};
+
 const Shop = () => {
-    return <h1>Shop</h1>;
+    const [cart, setCart] = useOutletContext();
+
+    const { products, error, loading } = useProducts();
+
+    const addToCart = (newItem, quantity) => {
+        if (quantity === 0 || !quantity) {
+            return;
+        }
+        const itemIndex = cart.findIndex((item) => item.id === newItem.id);
+        if (itemIndex !== -1) {
+            const updatedCart = [...cart];
+            updatedCart[itemIndex].quantity += quantity;
+            setCart(updatedCart);
+        } else {
+            setCart([...cart, { ...newItem, quantity }]);
+        }
+    };
+
+    if (loading) return <h2>Loading...</h2>;
+    if (error) return <h3>A network error has occured</h3>;
+
+
+    const productList = products.map((product) => {
+        return <Card key={product.id} product={product} onAddToCart={addToCart} />;
+    });
+
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.header}>Shop</h2>
+            <div className={styles.productGrid}>
+                {productList}
+            </div>
+        </div>
+    );
+
 };
 
 export default Shop;
