00f6e171fd5f62de998207e59524a4d57d7f0ff9
alex-delia <adelia3@uwo.ca>
2024-05-08 00:09:35 -0400

    create homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,15 @@
+import styles from '../styles/Home.module.css';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className={styles.container}>
+            <h1>Home</h1>
+            <h2>Welcome to the Store!</h2 >
+            <Link to='shop' className={styles.link}>Go To Shop</Link>
+        </div >
+    );
+
 };
 
 export default Home;
